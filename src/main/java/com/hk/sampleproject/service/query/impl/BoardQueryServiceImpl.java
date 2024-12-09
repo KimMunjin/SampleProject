ab6dfad5602aca2869311353f0207d2da8672df3
Kim Munjin <reminisce057@gmail.com>
2024-12-09 13:28:10 +0900

    refactor : Transactional 추가

--- src/main/java/com/hk/sampleproject/service/query/impl/BoardQueryServiceImpl.java
+++ src/main/java/com/hk/sampleproject/service/query/impl/BoardQueryServiceImpl.java
@@ -7,16 +7,19 @@ import com.hk.sampleproject.service.query.BoardQueryService;
 import com.hk.sampleproject.vo.BoardVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BoardQueryServiceImpl implements BoardQueryService {
 
     private final BoardQueryMapper boardQueryMapper;
 
+    @Transactional
     @Override
     public BoardDetailResponse getBoard(Long boardId) {
         BoardVo boardVo = boardQueryMapper.selectBoard(boardId);
